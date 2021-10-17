81b5d338b9f91081df8a2a11b2ce47c83b81e345
Chingiz Agha <cingiz167@gmail.com>
2021-10-17 19:51:49 +0400

    First Update

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.player1Score = new System.Windows.Forms.Label();
+            this.player2Score = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.Yellow;
+            this.player1.Location = new System.Drawing.Point(12, 129);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(27, 127);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // player2
+            // 
+            this.player2.BackColor = System.Drawing.Color.Fuchsia;
+            this.player2.Location = new System.Drawing.Point(795, 129);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(27, 127);
+            this.player2.TabIndex = 1;
+            this.player2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Location = new System.Drawing.Point(396, 166);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 26);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // player1Score
+            // 
+            this.player1Score.AutoSize = true;
+            this.player1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1Score.ForeColor = System.Drawing.Color.Lime;
+            this.player1Score.Location = new System.Drawing.Point(188, 9);
+            this.player1Score.Name = "player1Score";
+            this.player1Score.Size = new System.Drawing.Size(30, 24);
+            this.player1Score.TabIndex = 3;
+            this.player1Score.Text = "00";
+            // 
+            // player2Score
+            // 
+            this.player2Score.AutoSize = true;
+            this.player2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2Score.ForeColor = System.Drawing.Color.Red;
+            this.player2Score.Location = new System.Drawing.Point(630, 9);
+            this.player2Score.Name = "player2Score";
+            this.player2Score.Size = new System.Drawing.Size(30, 24);
+            this.player2Score.TabIndex = 4;
+            this.player2Score.Text = "00";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(834, 411);
+            this.Controls.Add(this.player2Score);
+            this.Controls.Add(this.player1Score);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox cpu;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.Label cpuScore;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label player1Score;
+        private System.Windows.Forms.Label player2Score;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool isWorking = true;
+        bool goUp;
+        bool goDown;
+        int speed = 10;
+        int score = 0;
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (goUp == true && player1.Location.Y > -1)
+            {
+                player1.Location = new Point(player1.Location.X, player1.Location.Y - speed);
+            }
+            if (goDown == true && player1.Location.Y < 283)
+            {
+                player1.Location = new Point(player1.Location.X, player1.Location.Y + speed);
+            }
+
+            if(isWorking == true)
+            {
+                player2.Location = new Point(player2.Location.X, player2.Location.Y + speed);
+                isWorking = false;
+            }
+            if (player2.Location.Y > -1 && player2.Location.Y < 129)
+            {
+                player2.Location = new Point(player2.Location.X, player2.Location.Y - speed);
+            }
+            if (player2.Location.Y < 283 && player2.Location.Y > 129)
+            {
+                player2.Location = new Point(player2.Location.X, player2.Location.Y + speed);
+            }
+
+        }
+    }
+}
